fcf7ad288c47a4e0f86eaeef4c88fdde7cc4d82b
silverlak3 <freefastfire@gmail.com>
2020-02-20 00:25:21 -0800

    Rafa Commit
    Added D2 Registration

---
+++ js/d2registration.js
@@ -0,0 +1,8 @@
+window.bboxInit = function () {
+  bbox.showForm('4f0daac2-91a5-4f5e-9fcf-937f95dfe1fb');
+  }; (function () {
+    var e = document.createElement('script');
+    e.async = true;
+    e.src = 'https://bbox.blackbaudhosting.com/webforms/bbox-min.js';
+    document.getElementsByTagName('head')[0].appendChild(e);
+    } ());
